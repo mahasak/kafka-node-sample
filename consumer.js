34c1232ab5ed23ebe5458788f30ab66bcf779bb8
Mahasak Pijittum <mahasak@gmail.com>
2018-05-13 03:31:32 +0700

    reformatting

--- consumer.js
+++ consumer.js
@@ -1,21 +1,21 @@
-var kafka = require('kafka-node'),
-    Consumer = kafka.Consumer,
-    client = new kafka.Client(),
-    consumer = new Consumer(client,
-        [{ topic: 'test-kafka', offset: 0}],
-        {
-            autoCommit: false
-        }
-    );
+const kafka = require('kafka-node')
+const Consumer = kafka.Consumer
+const client = new kafka.Client()
+const consumer = new Consumer(client, [{
+  topic: 'test-kafka',
+  offset: 0
+}], {
+  autoCommit: false
+});
 
 consumer.on('message', function (message) {
-    console.log(message);
+  console.log(message);
 });
 
 consumer.on('error', function (err) {
-    console.log('Error:',err);
+  console.log('Error:', err);
 })
 
 consumer.on('offsetOutOfRange', function (err) {
-    console.log('offsetOutOfRange:',err);
+  console.log('offsetOutOfRange:', err);
 })
